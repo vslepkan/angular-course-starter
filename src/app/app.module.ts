cec6e7b51351e7e010196e94e6c17a309712adb9
Vladyslav Slepkan <vladyslav.slepkan@intellectsoft.net>
2017-10-09 15:02:07 +0300

    Add 1st homework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherWidgetComponent } from './weather-widget/weather-widget.component';
+import { SocialWidgetComponent } from './social-widget/social-widget.component';
+import { ListWidgetComponent } from './list-widget/list-widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherWidgetComponent,
+    SocialWidgetComponent,
+    ListWidgetComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/social-widget/social-widget.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-social-widget',
+  templateUrl: './social-widget.component.html',
+  styleUrls: ['./social-widget.component.css']
+})
+export class SocialWidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
