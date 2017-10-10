2ce4f3980e4d5342dedcbbc803d1be4fb05bd558
Vladyslav Slepkan <vladyslav.slepkan@intellectsoft.net>
2017-10-10 13:50:45 +0300

    Add proper type for pipe

--- src/app/common/pipes/filter-type.pipe.spec.ts
+++ src/app/common/pipes/filter-type.pipe.spec.ts
@@ -1,8 +1,9 @@
 import { FilterTypePipe } from './filter-type.pipe';
+import { PipeTransform } from '@angular/core';
 
 describe('FilterTypePipe', () => {
   it('create an instance', () => {
-    const pipe = new FilterTypePipe();
+    const pipe: PipeTransform = new FilterTypePipe();
     expect(pipe).toBeTruthy();
   });
 });
--- src/app/common/pipes/filter-type.pipe.ts
+++ src/app/common/pipes/filter-type.pipe.ts
@@ -11,7 +11,7 @@ export class FilterTypePipe implements PipeTransform {
     }
 
     return value
-      .filter(({ type }) =>
+      .filter(({ type }: { type: string }) =>
         type.toLowerCase() === arg.toLowerCase());
   }
 }
